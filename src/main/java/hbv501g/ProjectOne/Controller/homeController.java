b5418974a67ea9c5898ccff33c867c4c3e489464
hjoddi <hjoddi@gmail.com>
2022-10-23 01:41:56 +0000

    JavaDoc for homeController

--- src/main/java/hbv501g/ProjectOne/Controller/homeController.java
+++ src/main/java/hbv501g/ProjectOne/Controller/homeController.java
@@ -16,18 +16,34 @@ import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * General controller class for all aspects of the website other than the user.
+ */
 @Controller
 public class homeController {
+    /**
+     * Variables.
+     */
     private RecipeService recipeService;
     private UserService userService;
+
+    /**
+     * Constructor for the homeController.
+     * @param recipeService - The recipe service which is used to manage the recipes.
+     * @param userService - The user service which is used to manage the users.
+     */
     @Autowired
     public homeController(RecipeService recipeService, UserService userService){
         this.recipeService = recipeService;
         this.userService = userService;
     }
 
-    /** Mapping of the front page. **/
+    /**
+     * Mapping of the front page.
+     * @param session - A HttpSession object, used to get information about the current session.
+     * @param model - The model currently being used.
+     * @return - Returns a String containing the name of the template to be displayed (index.html for this method).
+     */
     @GetMapping({"/", "/index"})
     public String homePage(HttpSession session, Model model) {
 
@@ -41,6 +57,9 @@ public class homeController {
      *  The search-parameters are retrieved and the database is
      *  queried for the recipes that match, which is then attached
      *  to the model which is then sent to the html-template to be displayed.
+     *  @param search - The SearchModel object used to search the database.
+     *  @param session - A HttpSession object, used to get information about the current session.
+     *  @param model - The model currently being used.
      */
     @RequestMapping(value = "/recipePage", method = RequestMethod.POST)
     public String SearchSubmitPOST(SearchModel search, HttpSession session, Model model){
@@ -63,6 +82,10 @@ public class homeController {
      *  database for the recipe with the matching id and adds
      *  it to the model. This model is then sent to the
      *  html-template to be displayed.
+     *  @param id - The id of the recipe to be displayed.
+     *  @param session - A HttpSession object, used to get information about the current session.
+     *  @param model - The model currently being used.
+     *  @return - A string containing the name of the template to be displayed (singleRecipePage.html in this case).
      */
     // TODO: Error message or when id with no match.
     @RequestMapping(value = "/singleRecipePage/{id}", method = RequestMethod.GET)
