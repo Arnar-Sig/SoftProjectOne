d412f193f60829dd4590264a78f3fe0bafc13478
Arnar-Sig <enteko5@gmail.com>
2022-11-20 22:44:53 +0000

    swapped database from h2 to postgresql

--- src/main/java/hbv501g/ProjectOne/Main.java
+++ src/main/java/hbv501g/ProjectOne/Main.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import java.util.HashSet;
