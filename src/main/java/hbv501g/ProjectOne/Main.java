88b87d4d609eb2127edec02c34624847d364ebd4
Arnar-Sig <enteko5@gmail.com>
2022-11-22 08:21:24 +0000

    first attempt at deployment

--- src/main/java/hbv501g/ProjectOne/Main.java
+++ src/main/java/hbv501g/ProjectOne/Main.java
@@ -26,10 +26,14 @@ public class Main {
 		SpringApplication.run(Main.class);
 	}
 
+
+
 	/**
 	 * Temporary code for adding recipes to the database.
 	 */
-/*
+
+
+	/*
 	// ONLY USE TO ADD NEW RECIPES //
 	@Bean
 	public CommandLineRunner demo(RecipeRepository repository) {
@@ -42,18 +46,18 @@ public class Main {
 			HashSet<String> comms = new HashSet<>();
 			HashSet<String> ingredients = new HashSet<>(); ingredients.add("Rice"); ingredients.add("Milk"); ingredients.add("Raisins");
 			Recipe r = new Recipe("Porridge", ingredients, "Add small amount of water and bring it to a boil. " +
-					"Then add the rice and milk and let it cook at low heat. Add raisings once it starts getting thicker.", comms);
+					"Then add the rice and milk and let it cook at low heat. Add raisings once it starts getting thicker.");
 			repo.save(r);
 			HashSet<String> ingredients2 = new HashSet<>(); ingredients2.add("Minced-Meat"); ingredients2.add("Spaghetti"); ingredients2.add("Pasta-Sauce");
-			repo.save(new Recipe("Spaghetti", ingredients2, "Boil spaghetti. Cook the meat until brown and add sauce. Combine.", comms));
+			repo.save(new Recipe("Spaghetti", ingredients2, "Boil spaghetti. Cook the meat until brown and add sauce. Combine."));
 			HashSet<String> ingredients3 = new HashSet<>(); ingredients3.add("Tuna"); ingredients3.add("Pasta"); ingredients3.add("Egg"); ingredients3.add("Any-vegetable");
-			repo.save(new Recipe("Tuna-Egg-Pasta", ingredients3, "Boil eggs and pasta for 10 minutes. Combine in a bowl and add tuna and veggies.", comms));
+			repo.save(new Recipe("Tuna-Egg-Pasta", ingredients3, "Boil eggs and pasta for 10 minutes. Combine in a bowl and add tuna and veggies."));
 			HashSet<String> ingredients4 = new HashSet<>(); ingredients4.add("Noodles"); ingredients4.add("Cheese"); ingredients4.add("Egg");
 			repo.save(new Recipe("Slightly better noodles", ingredients4, "Fry an egg. Put noodles and sauce in a pan. " +
-					"Sprinkle cheese on top and let it melt. Put egg on top.", comms));
+					"Sprinkle cheese on top and let it melt. Put egg on top."));
 
 			HashSet<String> ingredients5 = new HashSet<>(); ingredients5.add("Mud"); ingredients5.add("Leaves"); ingredients5.add("Oats");
-			repo.save(new Recipe("Test recipe", ingredients5, "Eat everything raw and unmixed, you animal.", comms));
+			repo.save(new Recipe("Test recipe", ingredients5, "Eat everything raw and unmixed, you animal."));
 
 		};
 	}
