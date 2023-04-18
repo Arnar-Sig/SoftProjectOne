838d8d67e82469a5c63ee31cf6d9823a7b43d30e
Arnar-Sig <enteko5@gmail.com>
2023-04-18 18:23:38 +0000

    Added a way to receive data from frontend client

--- src/main/java/hbv501g/ProjectOne/Controller/RecipeRestController.java
+++ src/main/java/hbv501g/ProjectOne/Controller/RecipeRestController.java
@@ -5,6 +5,7 @@ import hbv501g.ProjectOne.Repositories.RecipeRepository;
 import hbv501g.ProjectOne.Services.Implementation.RecipeServiceImplementation;
 import hbv501g.ProjectOne.Services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
 
@@ -21,12 +22,18 @@ public class RecipeRestController {
         this.recipeService = recipeservice;
     }
 
-    @GetMapping("/restRecipes")
+    @RequestMapping(value = "/restRecipes", method = RequestMethod.GET)
     public String getRecipes() {
         Gson gson = new Gson();
         ArrayList<Recipe> recipes = (ArrayList<Recipe>) recipeService.findAll();
         //return recipeService.findAll();
         return gson.toJson(recipes);
+    }
+
+    @RequestMapping(value = "/restUpdateRecipe", method = RequestMethod.POST)
+    public ResponseEntity<String> updateRecipe(@RequestBody Recipe recipe) {
+
+        return ResponseEntity.ok("Recipe created successfully");
     }
 
 }
